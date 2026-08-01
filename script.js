a6bfa25380146c438b403f271f98df406a041559
gazimojib <mdmojibgazisss2005@gmail.com>
2026-08-01 17:15:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -92,3 +92,68 @@ function updateDashboard(){
     personnel.length;
 
 }
+// ===============================
+// Part-2 : Dashboard + Search
+// ===============================
+
+// Search
+searchInput.addEventListener("keyup", function () {
+
+    const keyword = this.value.toLowerCase();
+
+    const filtered = personnel.filter(person => {
+
+        return (
+            String(person["ID No"]).toLowerCase().includes(keyword) ||
+            String(person["NAME"]).toLowerCase().includes(keyword) ||
+            String(person["RANK"]).toLowerCase().includes(keyword) ||
+            String(person["TRADE"]).toLowerCase().includes(keyword) ||
+            String(person["COY"]).toLowerCase().includes(keyword) ||
+            String(person["DISTRICT"]).toLowerCase().includes(keyword)
+        );
+
+    });
+
+    renderTable(filtered);
+
+});
+
+// Dashboard
+function updateDashboard(){
+
+    const total = personnel.length;
+
+    const collected = personnel.filter(p =>
+        String(p["Collected?"]).trim() === "ডাটা পাওয়া গেছে"
+    ).length;
+
+    const remaining = total - collected;
+
+    document.getElementById("total").innerText = total;
+    document.getElementById("collected").innerText = collected;
+    document.getElementById("remaining").innerText = remaining;
+
+    console.log("Total:", total);
+    console.log("Collected:", collected);
+    console.log("Remaining:", remaining);
+
+}
+// Section Count
+
+document.getElementById("hqCount").innerText =
+personnel.filter(x=>x["COY"]=="HQ BN").length;
+
+document.getElementById("c59Count").innerText =
+personnel.filter(x=>x["COY"]=="59 COY").length;
+
+document.getElementById("c60Count").innerText =
+personnel.filter(x=>x["COY"]=="60 COY").length;
+
+document.getElementById("c61Count").innerText =
+personnel.filter(x=>x["COY"]=="61 COY").length;
+
+document.getElementById("mtplCount").innerText =
+personnel.filter(x=>x["COY"]=="MTPL").length;
+
+document.getElementById("emeCount").innerText =
+personnel.filter(x=>x["COY"]=="EME").length;
